f0b7ef9212b2b9b6a678f21a04688eb13ef5ca73
suraj-2142 <melshankares@gmail.com>
2022-01-22 20:09:39 +0530

    Git push contri

--- Suraj_main_Jan22.tf
+++ Suraj_main_Jan22.tf
@@ -16,7 +16,7 @@ provider "aws" {
 resource "aws_key_pair" "deployer" {
   key_name   = "Terrafor-Key-suraj"
   public_key = "SURAJ"
-  Gir push for 20 Jan 2022
+  Gir push for 22 Jan 2022
 //}*/
 
 resource "aws_instance" "Terra-Demo" {
