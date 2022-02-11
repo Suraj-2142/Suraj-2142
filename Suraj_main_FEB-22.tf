4349a61d3ccd245db0dd0f4f3d4490799e9ca4a3
suraj-2142 <melshankares@gmail.com>
2022-02-11 12:32:54 +0530

    GIT PUSH

--- Suraj_main_FEB-22.tf
+++ Suraj_main_FEB-22.tf
@@ -16,7 +16,7 @@ provider "aws" {
 resource "aws_key_pair" "deployer" {
   key_name   = "Terrafor-Key-suraj"
   public_key = "SURAJ"
-  Git push for 11 ---FEB 2022 Git push GIT "" "name" {
+  Git push for 11FEB 2022 Git push GIT "" "name" {
     
   }
 //}*/
