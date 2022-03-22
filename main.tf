3759c81dd32f61c9269881f71314665bf6a17adc
suraj-2142 <melshankares@gmail.com>
2022-03-22 10:54:18 +0530

    AWS provider changed

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "3.72.0"
+      version = "4.6.0"
     }
   }
 }
@@ -12,27 +12,31 @@ provider "aws" {
  
 }
 
-/*
-resource "aws_key_pair" "deployer" {
-  key_name   = "Terrafor-Key-suraj"
-  public_key = "SURAJ"
-  Git push for 11
+# /*
+# resource "aws_key_pair" "deployer" {
+#   key_name   = "Terrafor-Key-suraj"
+#   public_key = "SURAJ"
+#   Git push for 11
   
   
- 14 - MARCH 22  Git push GIT "" "name" {
+#  14 - MARCH 22  Git push GIT "" "name" {
     
-  }
-//}*/
+#   }
+# //}*/
 
-resource "aws_instance" "Terra-Demo" {
-  ami           = "ami-001089eb624938d9f"
-  instance_type = "t2.micro"
-  subnet_id = "subnet-01a956015fff8a630"
-  key_name = "SURAJ"
- // key_name = "aws_key_pair.deployer.key_name"
+# resource "aws_instance" "Terra-Demo" {
+#   ami           = "ami-001089eb624938d9f"
+#   instance_type = "t2.micro"
+#   subnet_id = "subnet-01a956015fff8a630"
+#   key_name = "SURAJ"
+#  // key_name = "aws_key_pair.deployer.key_name"
 
-  tags = {
-    Name = "TERRAFORM-EC2"
-    createdby= "terraform-2142"
-  }
-}
+#   tags = {
+#     Name = "TERRAFORM-EC2"
+#     createdby= "terraform-2142"
+#   }
+# }
+
+
+# Building VPC
+
